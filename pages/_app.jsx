b17b2f6ab0d9b4b28a2480452a5a6421dcb5d3eb
goztrk <me@gokhan.org.tr>
2023-12-06 15:04:22 +0300

    Export bootstrap JS

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,8 +1,17 @@
+import React from 'react';
+
 import { BaseLayout } from '@components/layouts/base';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import '@styles/globals.scss';
 
 function App({ Component, pageProps }) {
     const getLayout = Component.getLayout || ((page) => page);
+
+    React.useEffect(() => {
+        // Correct way to import bootstrap JS in the client side
+        require('bootstrap/dist/js/bootstrap.bundle.min.js');
+    }, []);
+
     return <BaseLayout>{getLayout(<Component {...pageProps} />)}</BaseLayout>;
 }
 
